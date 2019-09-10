8d3d969ba54534ad4d63d685fe400ff082f28371
John Wiedmann <john.wiedmann@gmail.com>
2019-09-10 17:47:29 -0400

    Added PrimeAndCompositeNumbers\CountFactors.  Had to use the reference material and originally got 92% because I didn't use a long.

---
+++ PrimeAndCompositeNumbers/CountFactors.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodilityTests.PrimeAndCompositeNumbers
+{
+    [TestClass()]
+    public class CountFactors
+    {
+        [TestMethod()]
+        public void solutionTest()
+        {
+            Assert.AreEqual(8, solution(24));
+        }
+
+
+
+        public int solution(int N)
+        {
+            long i = 1;
+            var factors = 0;
+
+            while (i * i < N)
+            {
+                if (N % i == 0)
+                    factors += 2;
+
+                i++;
+            }
+
+            // Watch for prime
+            if (i * i == N)
+                factors++;
+
+            return factors;
+        }
+    }
+}
