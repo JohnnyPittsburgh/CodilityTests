a4cf322186f17003edf2d6637a0e26ba85bfb3b4
John Wiedmann <john.wiedmann@gmail.com>
2019-09-11 19:16:09 -0400

    Added the GreedyAlgorithms\TieRopes example.  Took me a little time to understand why it could be greedy.

---
+++ GreedyAlgorithms/TieRopes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodilityTests.GreedyAlgorithms
+{
+    [TestClass()]
+    public class TieRopes
+    {
+        [TestMethod()]
+        public void solutionTest()
+        {
+            var a = new[] { 1, 2, 3, 4, 1, 1, 3 };
+            Assert.AreEqual(3, solution(4, a));
+
+        }
+
+        /// <summary>
+        /// https://app.codility.com/programmers/lessons/16-greedy_algorithms/tie_ropes/
+        /// </summary>
+        /// <param name="K"></param>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public int solution(int K, int[] A)
+        {
+            // Took a little time to understand why this can be greedy.
+            //  Only adjacent ropes can be tied. So you have to take what you
+            //  are given until it exceeds the length.  Then start again.
+            var ropes = 0;
+            var ropeLength = 0;
+
+            foreach (var segment in A)
+            {
+                // Keep tying ropes together until we exceed the desired length.
+                ropeLength = ropeLength + segment;
+                if (ropeLength < K) continue;
+
+                // Exceeded the desired length, start again.
+                ropes++;
+                ropeLength = 0;
+            }
+            
+            return ropes;
+        }
+    }
+}
---
+++ Johnny/Sandbox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodilityTests.Johnny
+{
+    [TestClass()]
+    public class Sandbox
+    {
+        [TestMethod()]
+        public void solutionTest()
+        {
+            RecursionPrint(5);
+        }
+
+        public void RecursionPrint(int k)
+        {
+            Console.WriteLine($"Recursion({k}) begin");
+            if (k == 0) return;
+            RecursionPrint(k - 1);
+            Console.WriteLine($"Recursion({k}) exit");
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="W">Sorted array of weights</param>
+        /// <param name="K">Canoe weight limit</param>
+        /// <returns></returns>
+        public int GreedyCanoeist(int[] W, int K)
+        {
+            var canoes = 0;
+            var j = 0;
+            var i = W.Length - 1;
+            while (j > i)
+            {
+                if (W[i] + W[j] < K)
+                    j += 1;
+
+                canoes++;
+                i -= 1;
+            }
+
+            return canoes;
+        }
+
+
+    }
+}
